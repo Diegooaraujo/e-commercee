8afd25510c607da22ebc997c8f82a50e409ceff0
Diegooaraujo <149812280+Diegooaraujo@users.noreply.github.com>
2024-09-04 19:25:06 -0300

    Create index.php

---
+++ e-commerce/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <script src="script.js" defer></script>
+    
+</head>
+<body>
+    <div class="navbar">
+        <div class="header">
+            <div class="logo">
+                <img src="./img/logo.png" alt="logotipo">
+            </div>
+            <nav class="nav">
+                <ul>
+                    <li id="switch"><i class="fa-solid fa-gamepad"></i> Nitendo </li>
+                    <li id="plays"><i class="fa-brands fa-playstation"></i> PlayStation</li>
+                    <li id="xbox"><i class="fa-brands fa-xbox"></i> Xbox</li>
+                    <li id="pc"> <i class="fa-solid fa-desktop"></i> Pc</li>
+                </ul>
+
+            </nav>
+            <div class="nav-icon">
+                <img id="list" src="./img/icon/list.svg" alt="">
+            </div>
+        </div>
+
+    </div>
+    <header >
+        <section class="slider">
+            <div class="slider-content">
+                <input type="radio" name="btn-radio" id="radio1" >
+                <input type="radio" name="btn-radio" id="radio2" >
+                <input type="radio" name="btn-radio" id="radio3" >
+                <!--imagens-->
+                <div class="slide-box primeiro">
+                    <a href="#"><img class="img-desktop" src="img/slide/nitendo.png" alt="slide 1"></a>
+                </div>
+
+                <div class="slide-box">
+                    <a href="#"><img class="img-desktop" src="img/slide/playstation.png" alt="slide 1"></a>
+                </div>
+
+                <div class="slide-box">
+                    <a href="#"><img class="img-desktop" src="img/slide/xbox.png" alt="slide 1"></a>
+                </div>
+
+                <!--nav-auto-->
+
+                <div class="nav-auto">
+                    <div class="auto-btn1"></div>
+                    <div class="auto-btn2"></div>
+                    <div class="auto-btn3"></div>
+                </div>
+
+                <div class="nav-manual">
+                    <label for="radio1" class="manual-btn"></label>
+                    <label for="radio2" class="manual-btn"></label>
+                    <label for="radio3" class="manual-btn"></label>
+                </div>
+            </div>
+
+        </section>
+
+    </header>
+    <section>
+        <div class="conteiner">
+            <div class="play">
+                <div class="jogos"><div class="img" id="god_of_war"></div></div>
+                <div class="jogos"><div class="img" id ="the_last_of_us"></div></div>
+                <div class="jogos"><div class="img" id="spiderman"></div></div>
+                <div class="jogos"><div class="img" ><p id="vermais">Ver todos os jogos desta coleção</p></div></div>
+                
+            </div>
+            <div class="nitendo">
+                <div class="jogos"><div class="img" id="superMario"></div></div>
+                <div class="jogos"><div class="img" id="pokemon"></div></div>
+                <div class="jogos"><div class="img" id="hyrule"></div></div>
+                <div class="jogos"><div class="img" ><p id="vermais">Ver todos os jogos desta coleção</p></div></div>
+            </div>
+            <div class="xbox">
+                <div class="jogos"><div class="img" id="fc24"></div></div>
+                <div class="jogos"><div class="img" id="payDay"></div></div>
+                <div class="jogos"><div class="img" id="seaOfThieves"></div></div>
+                <div class="jogos"><div class="img" ><p id="vermais">Ver todos os jogos desta coleção</p></div></div>
+            </div>
+            <div class="pc">
+                <div class="jogos"><div class="img" id="gta"></div></div>
+                <div class="jogos"><div class="img" id="valorant"></div></div>
+                <div class="jogos"><div class="img" id="read_dead"></div></div>
+                <div class="jogos"><div class="img" ><p id="vermais">Ver todos os jogos desta coleção</p></div></div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p>Siga-nos nas redes sociais</p>
+    </footer>
+</body>
+</html>
